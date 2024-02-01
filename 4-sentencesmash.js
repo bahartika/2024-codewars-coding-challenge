e4a1e6378c9bbf531eac99ad1c49b668d9b05aa4
bahartika <bahartikasatria271@gmail.com>
2024-02-01 10:49:28 +0700

    day 4

---
+++ 4-sentencesmash.js
@@ -0,0 +1,11 @@
+// coding challenge
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+const smash = (word) => word.join(" ");
+
+// function smash(word) {
+//   return word.join(" ");
+// }
+
+console.log(smash(["Bahartika", "Satria", "Wijaya"]));

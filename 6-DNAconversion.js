0a071732032cf148c46528edc2387bf404545f65
bahartika <bahartikasatria271@gmail.com>
2024-02-04 00:49:24 +0700

    day #6

---
+++ 6-DNAconversion.js
@@ -0,0 +1,11 @@
+// coding challenge
+// 6/366
+// https://www.codewars.com/kata/5556282156230d0e5e000089
+
+const DNAtoRNA = (dna) =>
+  dna
+    .split(``)
+    .map((el) => (el === `T` ? `U` : el))
+    .join(``);
+
+console.log(DNAtoRNA("TTTT"));

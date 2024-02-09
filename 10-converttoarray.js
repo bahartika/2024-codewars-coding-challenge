fe8a00eb33db322f581b1ae420db00075faa8cf8
bahartika <bahartikasatria271@gmail.com>
2024-02-09 20:21:49 +0700

    day #10

---
+++ 10-converttoarray.js
@@ -0,0 +1,9 @@
+// coding challenge
+// 9/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+const stringToArray = (string) => {
+  return string.split(" ");
+};
+
+console.log(stringToArray("I love arrays they are my favorite"));

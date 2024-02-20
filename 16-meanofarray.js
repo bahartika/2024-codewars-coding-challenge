e4a30f4d4aa07fbbff2ebe839008ab5a112f9727
bahartika <bahartikasatria271@gmail.com>
2024-02-20 15:24:05 +0700

    day #16

---
+++ 16-meanofarray.js
@@ -0,0 +1,15 @@
+// Coding Challenge
+// 15/366
+// https://www.codewars.com/kata/563e320cee5dddcf77000158
+
+const getAverage = (x) => Math.floor(x.reduce((acc, curr) => acc + curr) / x.length);
+
+// function getAverage(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result += x[i];
+//   }
+//   return Math.floor(result / x.length);
+// }
+
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));

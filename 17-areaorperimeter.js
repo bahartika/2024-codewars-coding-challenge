6037755f034be16e5b3c6ce7c91d1ec072fbf4f6
bahartika <bahartikasatria271@gmail.com>
2024-02-22 09:48:42 +0700

    day #17

---
+++ 17-areaorperimeter.js
@@ -0,0 +1,7 @@
+// Coding Challenge
+// 15/366
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+const areaOrPerimeter = (l, w) => (l === w ? l * w : (l + w) * 2);
+
+console.log(areaOrPerimeter(6, 10));

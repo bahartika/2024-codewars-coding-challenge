0f1b685abc2e9efc625f16e59dda8f2649486309
bahartika <bahartikasatria271@gmail.com>
2024-03-06 10:18:44 +0700

    day #30

---
+++ 30-calculator.js
@@ -0,0 +1,22 @@
+// Coding Challenge
+// 30/366
+// https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/javascript
+
+const add = (a, b) => a + b;
+
+const divide = (a, b) => a / b;
+
+const multiply = (a, b) => a * b;
+
+const mod = (a, b) => a % b;
+
+const exponent = (a, b) => a ** b;
+
+const subt = (a, b) => a - b;
+
+console.log(subt(1, 2));
+console.log(exponent(1, 2));
+console.log(mod(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(2, 1));
+console.log(add(1, 2));

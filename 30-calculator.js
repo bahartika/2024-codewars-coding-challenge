2dabaa92f070e256472dcdca96d107c757fc29f3
bahartika <bahartikasatria271@gmail.com>
2024-03-07 20:07:02 +0700

    day#31

--- 30-calculator.js
+++ 30-calculator.js
@@ -3,15 +3,10 @@
 // https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/javascript
 
 const add = (a, b) => a + b;
-
 const divide = (a, b) => a / b;
-
 const multiply = (a, b) => a * b;
-
 const mod = (a, b) => a % b;
-
 const exponent = (a, b) => a ** b;
-
 const subt = (a, b) => a - b;
 
 console.log(subt(1, 2));
---
+++ 31-messigoals.js
@@ -0,0 +1,5 @@
+// Coding Challenge
+// 31/366
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/solutions/javascript
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;

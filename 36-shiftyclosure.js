73fba237ab1428356bd499b7a8879d1b9a154523
bahartika <bahartikasatria271@gmail.com>
2024-03-15 15:06:28 +0700

    day #36

---
+++ 36-shiftyclosure.js
@@ -0,0 +1,7 @@
+// Coding Challenge
+// 36/366
+// https://www.codewars.com/kata/514aa0dc21607ae236000017/train/javascript
+// Shifty Closures
+
+const greetAbe = () => "Hello, Abe!";
+const greetBen = () => "Hello, Ben!";

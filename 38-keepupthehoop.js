76d7674ca84b23e2503cba8fe0a27bfc55b16cfa
bahartika <bahartikasatria271@gmail.com>
2024-03-15 15:18:53 +0700

    day #38

---
+++ 38-keepupthehoop.js
@@ -0,0 +1,8 @@
+// Coding Challenge
+// 38/366
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+// Keep up the hoop
+
+const hoopCount = (n) => (n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks");
+
+console.log(hoopCount(3));

6ce7e121ba95d69cc68bf487b551154268df6f1f
bahartika <bahartikasatria271@gmail.com>
2024-03-21 17:18:23 +0700

    day #38

---
+++ 38-oddoreven.js
@@ -0,0 +1,6 @@
+// Coding Challenge
+// 38/366
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+// Even or Odd
+
+const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");

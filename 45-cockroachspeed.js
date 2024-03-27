658dd5d33f426916a43d947d7c8a4e75abe8c42c
bahartika <bahartikasatria271@gmail.com>
2024-03-27 12:14:22 +0700

    day #45

---
+++ 45-cockroachspeed.js
@@ -0,0 +1,8 @@
+// Coding Challenge
+// 45/366
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+// Third angle of triangle
+
+const cockroachSpeed = (s) => Math.floor(s * (100000 / 3600));
+
+console.log(cockroachSpeed(1.08));

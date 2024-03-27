92130c54fe6ff346f1402dd2961d72317a132440
bahartika <bahartikasatria271@gmail.com>
2024-03-27 12:07:45 +0700

    day #44

---
+++ 44-thirdangle.js
@@ -0,0 +1,8 @@
+// Coding Challenge
+// 44/366
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+// Third angle of triangle
+
+const otherAngle = (a, b) => 180 - (a + b);
+
+console.log(otherAngle(30, 60));

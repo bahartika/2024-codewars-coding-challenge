2ef7e5b028027737cfdf7bd5147180b1e87c45ca
bahartika <bahartikasatria271@gmail.com>
2024-04-17 06:15:29 +0700

    day #47

---
+++ 47-setAlarm.js
@@ -0,0 +1,8 @@
+// Coding Challenge
+// 47/366
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+// L1:Set Alarm
+
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+console.log(setAlarm(true, false));

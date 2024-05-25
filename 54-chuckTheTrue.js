4c4642be4d431b23904c03f40f5ebe7ef6345e99
bahartika <bahartikasatria271@gmail.com>
2024-05-25 08:17:45 +0700

    day #54

---
+++ 54-chuckTheTrue.js
@@ -0,0 +1,6 @@
+// Coding Challenge
+// 54/366
+// https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
+//  Chuck Norris VII - True or False? (Beginner)
+
+const ifChuckSaysSo = () => !true;

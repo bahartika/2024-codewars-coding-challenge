af5a96f73952d745404fab8ca39b00b9da261f33
bahartika <bahartikasatria271@gmail.com>
2024-05-28 17:37:10 +0700

    day #56

---
+++ 56-paper.js
@@ -0,0 +1,8 @@
+// Coding Challenge
+// 56/366
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
+//  Beginner Series #1 School Paperwork
+
+const paperwork = (n, m) => (n > 0 && m > 0 ? n * m : 0);
+
+console.log(paperwork());

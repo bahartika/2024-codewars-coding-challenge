e1e22889196eec71f909a32e2e7d27edabd0985d
bahartika <bahartikasatria271@gmail.com>
2024-07-09 11:00:37 +0700

    day #58

---
+++ 58-uppercse.js
@@ -0,0 +1,7 @@
+// Coding Challenge
+// 58/366
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+const makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase("hello"));

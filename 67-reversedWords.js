4498ef2f272c4549f90563df68a7d33b770d4137
bahartika <bahartikasatria271@gmail.com>
2024-11-12 08:45:11 +0700

    day #67

---
+++ 67-reversedWords.js
@@ -0,0 +1,12 @@
+// Coding Challenge
+// 67/366
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
+
+console.log(reverseWords("hello world!"));
+console.log(reverseWords("yoda doesn't speak like this"));
+console.log(reverseWords("foobar"));
+console.log(reverseWords("kata editor"));
+console.log(reverseWords("row row row your boat"));
+console.log(reverseWords(""));

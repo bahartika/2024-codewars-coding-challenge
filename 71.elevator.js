7e384e8c67a6cac817321cfeae105836b3c065d7
bahartika <bahartikasatria271@gmail.com>
2024-12-24 14:17:02 +0700

    day #71

---
+++ 71.elevator.js
@@ -0,0 +1,22 @@
+// Coding Challenge
+// 71/366
+// https://www.codewars.com/kata/5748a883eb737cab000022a6/train/javascript
+
+// function elevator(left, right, call) {
+//   if(Math.abs(left - call) > Math.abs(right - call)){
+//     return "right"
+//   } else if (Math.abs(right - call) > Math.abs(left - call)){
+//     return "left"
+//   } else {
+//     return "right"
+//   }
+// }
+
+const elevator = (left, right, call) => (Math.abs(left - call) > Math.abs(right - call) || Math.abs(left - call) == Math.abs(right - call) ? "right" : "left");
+
+console.log(elevator(0, 1, 2));
+console.log(elevator(2, 1, 0));
+console.log(elevator(1, 0, 2));
+console.log(elevator(2, 0, 2));
+console.log(elevator(0, 0, 0));
+console.log(elevator(1, 1, 2));

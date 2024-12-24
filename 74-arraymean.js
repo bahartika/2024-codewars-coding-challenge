8d7ea90b55e03370790d8dc5142dfd47a2b59c78
bahartika <bahartikasatria271@gmail.com>
2024-12-24 15:53:09 +0700

    day #74

---
+++ 74-arraymean.js
@@ -0,0 +1,24 @@
+// Coding Challenge
+// 73/366
+// https://www.codewars.com/kata/570a6a46455d08ff8d001002/train/javascript
+
+// function findAverage(nums) {
+//   let sum = 0;
+//   for (i = 0; i < nums.length; i++) {
+//     sum += nums[i];
+//   }
+//   return sum / nums.length;
+// }
+
+// const findAverage = (nums) => {
+//   if (nums.length === 0) return 0;
+
+//   let sum = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+//   return sum / nums.length;
+// };
+
+const findAverage = (nums) => nums.reduce((accumulator, currentValue) => accumulator + currentValue) / nums.length;
+
+console.log(findAverage([1, 3, 5, 7]));
+console.log(findAverage([2, 4, 6, 8, 10]));
